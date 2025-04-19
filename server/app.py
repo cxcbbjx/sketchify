92330e10cc2be9b5ad0fe6c79b89d38c712db88b
shivang <shivangsagar529@gmail.com>
2025-04-20 01:35:02 +0530

    Fix: bind Flask to 0.0.0.0 for Render deployment

--- server/app.py
+++ server/app.py
@@ -46,4 +46,5 @@ def serve_sketch(filename):
     return send_from_directory(SKETCH_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(host="0.0.0.0", port=5000, debug=True)
+
